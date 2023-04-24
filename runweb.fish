88c89c95867229c463fbef137234aba2e2c0aaf0
Appuchia <appuchia@appu.ltd>
2023-04-25 01:07:54 +0200

    ftr: [main] Remove unused scripts

--- runweb.fish
+++
@@ -1,3 +0,0 @@
-#! /bin/fish
-cd /home/appu/appuwrotethese/
-pipenv run gunicorn
--- updatedb.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/fish
-
-cd /home/appu/appuwrotethese
-#. ./venv/bin/activate.fish
-python3 manage.py update_db
