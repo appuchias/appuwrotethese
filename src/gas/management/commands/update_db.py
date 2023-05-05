06116e2e948111061660c0d7f0a2bcf597e0690d
Appuchia <appuchia@appu.ltd>
2023-05-05 09:27:10 +0200

    ftr: [gas] Rename db_updater to db_actions

--- src/gas/management/commands/update_db.py
+++ src/gas/management/commands/update_db.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from gas import db_updater
+from gas import db_actions
 
 
 class Command(BaseCommand):
     help = "Updates the database for the gas app"
 
     def handle(self, *args, **options):
-        db_updater.update_db()
+        db_actions.update_db()
