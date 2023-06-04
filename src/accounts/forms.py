8a9637df26d7b29442779543441050d131a6a6ce
Appuchia <appuchia@appu.ltd>
2023-06-04 20:12:08 +0200

    feat: [acc] Add license headers

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,3 +1,19 @@
+# Appu Wrote These
+# Copyright (C) 2023  Appuchia <appuchia@appu.ltd>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.contrib.auth.forms import (
     AuthenticationForm,
     PasswordChangeForm,
@@ -9,6 +25,7 @@ from django.utils.translation import gettext_lazy as _
 from crispy_forms.layout import Layout, Div, Submit, HTML
 from crispy_forms.helper import FormHelper
 from accounts.models import AWTUser
+
 
 # User creation form for the web
 class AWTUserCreationForm(UserCreationForm):
@@ -157,7 +174,6 @@ class AWTPasswordResetForm(forms.Form):
 
 
 class AWTUpgradeForm(forms.Form):
-
     upgrade_uuid = forms.CharField(
         label=_("Upgrade UUID"),
         help_text=_(
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,19 @@
+# Appu Wrote These
+# Copyright (C) 2023  Appuchia <appuchia@appu.ltd>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
@@ -6,9 +22,7 @@ from gas.models import Station
 
 
 class AWTUser(User):
-    """
-    Model for storing users
-    """
+    """Model for storing users"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
