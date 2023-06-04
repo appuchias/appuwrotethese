a385671ebdee5ade2aa5e0bea786200faf1b61e8
Appuchia <appuchia@appu.ltd>
2023-06-04 23:44:02 +0200

    feat: Filter permanent messages by path

--- src/middleware/permanent_messages_middleware.py
+++ src/middleware/permanent_messages_middleware.py
@@ -2,8 +2,12 @@ from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
 MESSAGES = [
-    # {"type": "info", "text": _("The web is under development. It HAS bugs")},
-    {"type": "warning", "text": _("Province searching might be broken") + "."},
+    # {"type": "info", "text": _("The web is under development. It HAS bugs"), "path": ""},
+    {
+        "type": "warning",
+        "text": _("Province searching might be broken") + ".",
+        "path": "gas",
+    },
 ]
 
 
@@ -20,11 +24,12 @@ class PermanentMessagesMiddleware(object):
 
     def __call__(self, request):
         # Here code before view
-        if len(messages.get_messages(request)) < len(MESSAGES):
+        if len(messages.get_messages(request)) < len(MESSAGES) and request.path != "/":
             for message in MESSAGES:
-                messages.add_message(
-                    request, self.levels[message["type"]], message["text"]
-                )
+                if message.get("path", "") in request.path:
+                    messages.add_message(
+                        request, self.levels[message["type"]], message["text"]
+                    )
 
         response = self.get_response(request)
         # Here code after view
