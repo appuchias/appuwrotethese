09e084e8ff548e16e9824e73a6a8ed49325579c0
Appuchia <appuchia@appu.ltd>
2023-06-15 13:07:19 +0200

    Revert "feat: Disable accounts app"
    This reverts commit 62f30bbd39166e49f96a6cb0e496b7cdad1df061.

--- src/appuwrotethese/urls.py
+++ src/appuwrotethese/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     path("api/", include("api.urls")),
     path("gas/", include("gas.urls")),
-    # path("accounts/", include("accounts.urls")),
+    path("accounts/", include("accounts.urls")),
     path("i18n/", include("django.conf.urls.i18n"), name="i18n"),
     path("robots.txt", views.redirect_static, kwargs={"resource": "robots.txt"}),
     path("favicon.ico", views.redirect_static, kwargs={"resource": "favicon.ico"}),
