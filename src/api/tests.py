6925b341f43a3f063438d38e1fb7e3957c20094c
Appuchia <appuchia@appu.ltd>
2023-06-30 18:40:11 +0200

    fix: [api] Fix tests

--- src/api/tests.py
+++ src/api/tests.py
@@ -22,16 +22,16 @@ class APITests(TestCase):
     def test_api(self):
         response = self.client.get("/api/")
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response.json(), "Welcome to the API!")
+        self.assertContains(response, "Welcome to the API!")
 
     def test_health(self):
         response = self.client.get("/api/health/")
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response.json(), "OK")
+        self.assertContains(response, "OK")
 
 
 class APIGasTests(TestCase):
     def test_api_gas(self):
         response = self.client.get("/api/gas/")
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response.json(), "Welcome to the API!")
+        self.assertContains(response, "Welcome to the Gas API")
