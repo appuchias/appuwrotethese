2d5c0f50b4bbe5bbfc895e8d26440d63c15515f5
Appuchia <appuchia@appu.ltd>
2024-04-22 19:12:14 +0200

    feat: PWA base setup

---
+++ src/staticfiles/sw.js
@@ -0,0 +1,7 @@
+// Service worker para instalar la PWA
+self.addEventListener("install", event => {
+    console.log("Service worker installed");
+});
+self.addEventListener("activate", event => {
+    console.log("Service worker activated");
+})
